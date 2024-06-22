5f240a7413c9f07aa4b5c1e9e5a799e3cc9c4665
Drew Weisenberg <weisenbergd@gmail.com>
2024-06-21 20:54:28 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { generateArray } from "./lib/generate-array";
+
+function App() {
+  generateArray(1);
+
+  return (
+    <>
+      <h1 className="text-xxl">Hello world!</h1>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/lib/generate-array.tsx
@@ -0,0 +1,5 @@
+export function generateArray(size: number, type: string = "number") {
+  console.log(`array type = ${type}`);
+  const array: [] = [];
+  console.log("test");
+}
